975bc08a77182ce7445b1c89a3c27dbcfcbe5381
__tp__ <mynameistp16@gmail.com>
2018-11-13 08:01:13 +0530

    Add files via upload
    sample code to check db connection

---
+++ mysql.py
@@ -0,0 +1,15 @@
+import MySQLdb
+
+try:
+    db = MySQLdb.connect(host="localhost", user="root", passwd="", db="test")
+
+    curs = db.cursor()
+
+    curs.execute("select * from Students")
+
+    for row in curs.fetchall():
+        print("Name: %s, Address: %s" % (row[1], row[2]))
+
+except Exception as e:
+    print(e)
+
